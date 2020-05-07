7922b59e97351a7fdc4c4c313860290d69062962
kik-doctor <topwolf0808@gmail.com>
2020-05-07 15:36:32 +0800

    Fix env variable

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'http://3.105.143.8api/',
+  BASE_URL: 'http://3.105.143.8/api/',
   API_URL: 'http://3.105.143.8/api/root/',
   AUTH_API: 'http://3.105.143.8/api/auth/',
 };
